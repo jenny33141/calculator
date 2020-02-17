5403a44bf240166647f8817de35a6a5ccf670c7d
CN23 <codenation@CN23s-MacBook-Pro.local>
2020-02-17 16:28:01 +0000

    updated state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,60 @@ import './App.css';
 
 class App extends Component {
   state = {
-    h1: 0
+    h1: ""
+    // h1: ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", ".", "-", "+","*", "/", "=", "AC" ]
+    } 
+
+    clickOne = (num) => {
+      let sum = this.state.h1
+      sum += num
+      
+      this.setState ({h1: sum})
     }
 
-    clickOne = () => {
-      this.setState ({h1:this.state.h1 + 1})
-    }
+clear = () => {
+  // console.log ("cleared")
+  this.setState({h1:""})
+}
+
+evaluate = () => {
+try {
+  let result = eval (this.state.h1)
+this.setState({h1:result})
+} catch (error) {
+  console.log(error)
+}
+}
+
 
   render () {
     return (
-      <div>
+      <div className="wrapper">
+      <div className="calc">
+        <div className ="display">
       <h1>{this.state.h1}</h1>
-      
+      </div>
+      <div className ="buttonContainer">
+      <button className="button" name="1" onClick={() => this.clickOne("1")}>1</button>
+      <button className="button" name="1" onClick={() => this.clickOne("2")}>2</button>
+      <button className="button" name="1" onClick={() => this.clickOne("3")}>3</button>
+      <button className="button" name="1" onClick={() => this.clickOne("-")}>-</button>
+      <button className="button" name="1" onClick={() => this.clickOne("4")}>4</button>
+      <button className="button" name="1" onClick={() => this.clickOne("5")}>5</button>
+      <button className="button" name="1" onClick={() => this.clickOne("6")}>6</button>
+      <button className="button" name="1" onClick={() => this.clickOne("+")}>+</button>
+      <button className="button" name="1" onClick={() => this.clickOne("7")}>7</button>
+      <button className="button" name="1" onClick={() => this.clickOne("8")}>8</button>
+      <button className="button" name="1" onClick={() => this.clickOne("9")}>9</button>
+      <button className="button" name="1" onClick={() => this.clickOne("*")}>*</button>
+
+      <button className="button" id="clButton" name="1" onClick={this.clear}>AC</button>
+      <button className="button" name="1" onClick={() => this.clickOne("0")}>0</button>
+      <button className="button" name="1" onClick={() => this.clickOne(".")}>.</button>
+      <button className="button" name="1" onClick={() => this.clickOne("/")}>/</button>
+      <button className="button" id="equals" name="1" onClick={this.evaluate}>=</button>
+      </div>
+      </div>
       </div>
     )
   } 
