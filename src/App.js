25589193b960bb295c04b6615630c7ee0c59d391
jenny33141 <jennylewis33@hotmail.co.uk>
2020-02-26 16:30:30 +0000

    improved code commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,99 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   state = {
-    h1: ""
-    // h1: ["0", "1", "2", "3","4", "5", "6", "7", "8", "9", ".", "-", "+","*", "/", "=", "AC" ]
-    } 
+    h1: "",
+    buttons: [
+      "+",
+      "-",
+      "*",
+      "/",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      ".",
+      "0"
+    ]
+  };
 
-    clickOne = (num) => {
-      let sum = this.state.h1
-      sum += num
-      
-      this.setState ({h1: sum})
+  clickOne = num => {
+    console.log("Do you work?");
+    let sum = this.state.h1;
+    sum += num;
+
+    this.setState({ h1: sum });
+  };
+
+  clear = () => {
+    console.log("cleared");
+    this.setState({ h1: "" });
+  };
+
+  evaluate = () => {
+    try {
+      let result = eval(this.state.h1);
+      this.setState({ h1: result });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-clear = () => {
-  // console.log ("cleared")
-  this.setState({h1:""})
-}
+  // mainHandler = button => {
+  //   if (button == "clear") {
+  //     console.log("delete");
+  //   }
 
-evaluate = () => {
-try {
-  let result = eval (this.state.h1)
-this.setState({h1:result})
-} catch (error) {
-  console.log(error)
-}
-}
-
-
-  render () {
+  render() {
     return (
       <div className="wrapper">
-      <div className="calc">
-        <div className ="display">
-      <h1>{this.state.h1}</h1>
+        <div className="calc">
+          <div className="display">{this.state.h1}</div>
+          <div className="buttonContainer">
+            {this.state.buttons.map(num => {
+              return (
+                <button
+                  className="button"
+                  onClick={() => this.clickOne(`${num}`)}
+                >
+                  {num}
+                </button>
+              );
+            })}
+
+            <button
+              className="button"
+              id="clButton"
+              name="1"
+              onClick={this.clear}
+            >
+              AC
+            </button>
+            <button
+              className="button"
+              id="equals"
+              name="1"
+              onClick={this.evaluate}
+            >
+              =
+            </button>
+          </div>
+        </div>
       </div>
-      <div className ="buttonContainer">
-      <button className="button" name="1" onClick={() => this.clickOne("1")}>1</button>
+    );
+  }
+}
+
+export default App;
+
+/* <button className="button" name="1" onClick={() => this.clickOne("1")}>1</button>
       <button className="button" name="1" onClick={() => this.clickOne("2")}>2</button>
       <button className="button" name="1" onClick={() => this.clickOne("3")}>3</button>
       <button className="button" name="1" onClick={() => this.clickOne("-")}>-</button>
@@ -58,22 +113,4 @@ this.setState({h1:result})
       <button className="button" id="equals" name="1" onClick={this.evaluate}>=</button>
       </div>
       </div>
-      </div>
-    )
-  } 
-  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-export default App;
+      </div> */
