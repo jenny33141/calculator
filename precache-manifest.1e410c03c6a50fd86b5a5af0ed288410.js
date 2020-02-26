ad13b765eadf636ff6f7ce77d795870e4b217941
jenny33141 <jennylewis33@hotmail.co.uk>
2020-02-26 17:03:43 +0000

    Updates

---
+++ precache-manifest.1e410c03c6a50fd86b5a5af0ed288410.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "194bd3c27abbaad4dd2b0212a8710477",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "77063fa7eca120456722",
+    "url": "/calculator/static/css/main.b8128f46.chunk.css"
+  },
+  {
+    "revision": "439c54393768db564794",
+    "url": "/calculator/static/js/2.22fff6a2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.22fff6a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77063fa7eca120456722",
+    "url": "/calculator/static/js/main.ef044114.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
